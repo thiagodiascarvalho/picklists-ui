b20ee0dadc9288881a7b5c13b13829222dc4b84d
MatheusMariano <matheusmariano@nasajon.com.br>
2023-01-17 19:24:05 -0300

    Correção ativação botão

--- lib/repositories/selected_picklists_repository.dart
+++ lib/repositories/selected_picklists_repository.dart
@@ -7,10 +7,12 @@ class SelectedPickListRepository {
 
   static void addPickListId(pickListId) {
     selectedPickLists.add(pickListId);
+    selectedPickListsNotifier.notifyListeners();
   }
 
   static void removePickListId(pickListId) {
     selectedPickLists.remove(pickListId);
+    selectedPickListsNotifier.notifyListeners();
   }
 
   static void clear() {
